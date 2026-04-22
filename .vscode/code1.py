c8eb928df8c5e5e5c358124a2084e9341b539150
Nezox4509 <mf_mss@mail.ru>
2026-04-22 16:15:44 +0900

    3

--- .vscode/code1.py
+++ .vscode/code1.py
@@ -1,10 +1,4 @@
-echo "# code" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Nezox4509/code.git
-git push -u origin main
+
 
 import subprocess
 import platform
